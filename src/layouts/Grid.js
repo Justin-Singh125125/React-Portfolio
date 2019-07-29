61468e983293257aa4205edde819d687d3d68f9d
Justin Singh <justin.singh125125@gmail.com>
2019-07-28 22:02:03 -0700

    getting stuff set up

---
+++ src/layouts/Grid.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Grid = props => {
+    return (
+        <div className="grid">{props.children}</div>
+    )
+}
+
+export default Grid;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 //layouts
-import Introduction from "../../layouts/Introduction";
+import Grid from "../../layouts/Grid";
+import SectionIntroduction from "../../layouts/Introduction";
 
 //components
-import BackgroundVideo from "../../components/BackgroundVideo";
+
 
 
 
@@ -12,52 +13,17 @@ import BackgroundVideo from "../../components/BackgroundVideo";
 
 class Home extends React.Component {
 
-    state = {
-        videoNumber: 1
-    }
-
-    componentDidMount() {
-
-        this.handleBackgroundVideoTimer();
-    }
-
-
-
-
-    handleBackgroundVideoTimer = () => {
-
-        let currentVideoNumber = this.state.videoNumber;
-        let nextVideoNumber = 0;
-
-        if (currentVideoNumber === 1) {
-            nextVideoNumber = 2;
-        }
-        if (currentVideoNumber === 2) {
-            nextVideoNumber = 3;
-        }
-        if (currentVideoNumber === 3) {
-            nextVideoNumber = 1;
-        }
-
-
-        setTimeout(function () {
-            this.setState({
-                videoNumber: nextVideoNumber
-            }, () => {
-                this.handleBackgroundVideoTimer();
-            })
-        }.bind(this),
-            8000);
-    }
-
 
     render() {
 
 
         return (
-            <Introduction>
-                <BackgroundVideo videoNumber={this.state.videoNumber} />
-            </Introduction>
+            <Grid>
+                <SectionIntroduction>
+                    Intro
+                </SectionIntroduction>
+            </Grid>
+
         );
     }
 }
