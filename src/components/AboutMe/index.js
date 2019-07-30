d2bfda2f583ec2bd172d3f1a5f89e35035a970b8
Justin Singh <justin.singh125125@gmail.com>
2019-07-30 10:05:15 -0700

    implementing the about me info

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+//sub-components
+import Technologies from "./sub-components/technologies";
 const AboutMe = props => {
     return (
         <div className="about-me">
@@ -8,6 +10,8 @@ const AboutMe = props => {
                 Hello, I am a certified Full Stack Web Developer with a background in Computer Science and a lifelong dedication to learning. I am effective at combining creativity and problem-solving techniques to help develop user-friendly applications. Known among colleagues for implementing quality decisions into projects that reflect integrity to the client. I am pursuing a Masters degree in Computer Science.
                 I am experienced in the technologies found below!
             </p>
+
+            <Technologies />
         </div>
     )
 }
---
+++ src/components/AboutMe/sub-components/technologies.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Technologies = props => {
+    return (
+        <div className="about-me__technologies">
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Javascript</p>
+            <p>React</p>
+            <p>Mongo</p>
+            <p>MySQL</p>
+        </div>
+    )
+}
+
+export default Technologies;
