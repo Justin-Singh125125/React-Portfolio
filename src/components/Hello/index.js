0bea30245c28ba68547183c5c0b27fcb8eae46f0
Justin Singh <justin.singh125125@gmail.com>
2019-07-30 21:44:51 -0700

    touched up hello section

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 
+//components
+import SvgIcon from "../SvgIcon";
+
+
 const Hello = props => {
     return (
         <div className="hello">
             <h5 className="hello__im">Hello I'm</h5>
             <h2 className="hello__name">Justin Singh</h2>
             <h3 className="hello__stack">Full Stack Web Developer</h3>
-            <h6 className="hello__email">Justin.Singh125125@gmail.com</h6>
-            <h6 className="hello__number">(916)-955-8073</h6>
+            <h6 className="hello__email"><SvgIcon svgClass="hello__icon hello__email-icon" svgSelection="icon-envelop" />Justin.Singh125125@gmail.com</h6>
+            <h6 className="hello__number"><SvgIcon svgClass="hello__icon hello__phone-icon" svgSelection="icon-phone" />(916)-955-8073</h6>
             <div className="hello__icons">
-                {/* <span className="hello__icons--github">Github</span>
-                <span className="hello__icons--Linkedin">LinkedIn</span> */}
+                <div className="hello__icons--1">
+                    <a href="https://github.com/Justin-Singh125125" target="_blank">
+                        <SvgIcon svgClass="hello__icon hello__github-icon" svgSelection="icon-github" />
+                    </a>
+                </div>
+                <div className="hello__icons--2">
+                    <a href="https://www.linkedin.com/in/justin-singh-70a147178" target="_blank">
+                        <SvgIcon svgClass="hello__icon hello__linkedin-icon" svgSelection="icon-linkedin" />
+                    </a>
+                </div>
             </div>
 
         </div>
