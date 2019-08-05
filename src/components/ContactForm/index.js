24e39b8b87111dffcc966bf5c30359aae4f20e49
Justin Singh <justin.singh125125@gmail.com>
2019-08-05 16:44:28 -0700

    working on contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -7,8 +7,8 @@ const ContactForm = props => {
             <input type="text" className="contact__lastName" placeholder="First Name" />
             <input type="email" className="contact__email" placeholder="Your Email" />
 
-            <textarea className="contact__message" cols="30" rows="10"></textarea>
-
+            <textarea className="contact__message" cols="30" rows="10" placeholder="Your message"></textarea>
+            <button className="contact__btn btn btn--green">Send Message</button>
         </div>
     )
 }
