5b908577e636277694957053841a410a21c229e1
Justin Singh <justin.singh125125@gmail.com>
2019-08-06 12:15:22 -0700

    added footer

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Slide from 'react-reveal/Slide';
 
 //components
 import SvgIcon from "../SvgIcon";
@@ -7,25 +8,49 @@ import SvgIcon from "../SvgIcon";
 const Hello = props => {
     return (
         <div className="hello">
-            <h5 className="hello__im">Hello I'm</h5>
-            <h2 className="hello__name">Justin Singh</h2>
-            <h3 className="hello__stack">Full Stack Web Developer</h3>
-            <a className="hello__link" href="mailto:justin.singh125125@gmail.com"><h6 className="hello__email"><SvgIcon svgClass="hello__icon hello__email-icon" svgSelection="icon-envelop" />Justin.Singh125125@gmail.com</h6></a>
-            <a className="hello__link" href="tel:9169558073"><h6 className="hello__number"><SvgIcon svgClass="hello__icon hello__phone-icon" svgSelection="icon-phone" />(916)-955-8073</h6></a>
-            <div className="hello__icons">
-                <div className="hello__icons--1">
-                    <a href="https://github.com/Justin-Singh125125" target="_blank">
-                        <SvgIcon svgClass="hello__icon hello__github-icon" svgSelection="icon-github" />
-                    </a>
-                </div>
-                <div className="hello__icons--2">
-                    <a href="https://www.linkedin.com/in/justin-singh-70a147178" target="_blank">
-                        <SvgIcon svgClass="hello__icon hello__linkedin-icon" svgSelection="icon-linkedin" />
-                    </a>
-                </div>
-            </div>
 
-        </div>
+            <Slide left duration={1000}>
+                <h5 className="hello__im">Hello I'm</h5>
+
+
+                <h2 className="hello__name">Justin Singh</h2>
+                <h3 className="hello__stack">Full Stack Web Developer</h3>
+            </Slide>
+
+            <a className="hello__link" href="mailto:justin.singh125125@gmail.com">
+
+                <Slide right duration={1000}>
+                    <h6 className="hello__email">
+                        <SvgIcon svgClass="hello__icon hello__email-icon" svgSelection="icon-envelop" />Justin.Singh125125@gmail.com
+                        </h6>
+                </Slide>
+
+            </a>
+
+
+            <a className="hello__link" href="tel:9169558073">
+                <Slide right duration={1000}>
+                    <h6 className="hello__number">
+                        <SvgIcon svgClass="hello__icon hello__phone-icon" svgSelection="icon-phone" />(916)-955-8073
+                    </h6>
+                </Slide>
+            </a>
+            <Slide left duration={1000}>
+                <div className="hello__icons">
+                    <div className="hello__icons--1">
+                        <a href="https://github.com/Justin-Singh125125" target="_blank">
+                            <SvgIcon svgClass="hello__icon hello__github-icon" svgSelection="icon-github" />
+                        </a>
+                    </div>
+                    <div className="hello__icons--2">
+                        <a href="https://www.linkedin.com/in/justin-singh-70a147178" target="_blank">
+                            <SvgIcon svgClass="hello__icon hello__linkedin-icon" svgSelection="icon-linkedin" />
+                        </a>
+                    </div>
+                </div>
+            </Slide>
+
+        </div >
     )
 }
 
--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+import Bounce from 'react-reveal/Bounce';
+
 //photos
 import Hero from "../../assets/img/me.png"
 
 const HeroImage = props => {
     return (
         <>
-            <div className="hero">
-                <img src={Hero} alt="Justin Singh" className="hero__img" />
+            <Bounce right duration={2000}>
+                <div className="hero">
+                    <img src={Hero} alt="Justin Singh" className="hero__img" />
 
-            </div>
-            <div className="hero__border--1">&nbsp;</div>
-            <div className="hero__border--2">&nbsp;</div>
+                </div>
+                <div className="hero__border--1">&nbsp;</div>
+                <div className="hero__border--2">&nbsp;</div>
+            </Bounce>
 
         </>
 
