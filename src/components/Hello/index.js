64d5af53d81e405f95630fd88a8c70f91ae1c506
Justin Singh <justin.singh125125@gmail.com>
2019-08-05 20:01:06 -0700

    about to test

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -11,7 +11,7 @@ const Hello = props => {
             <h2 className="hello__name">Justin Singh</h2>
             <h3 className="hello__stack">Full Stack Web Developer</h3>
             <a className="hello__link" href="mailto:justin.singh125125@gmail.com"><h6 className="hello__email"><SvgIcon svgClass="hello__icon hello__email-icon" svgSelection="icon-envelop" />Justin.Singh125125@gmail.com</h6></a>
-            <h6 className="hello__number"><SvgIcon svgClass="hello__icon hello__phone-icon" svgSelection="icon-phone" />(916)-955-8073</h6>
+            <a className="hello__link" href="tel:9169558073"><h6 className="hello__number"><SvgIcon svgClass="hello__icon hello__phone-icon" svgSelection="icon-phone" />(916)-955-8073</h6></a>
             <div className="hello__icons">
                 <div className="hello__icons--1">
                     <a href="https://github.com/Justin-Singh125125" target="_blank">
