babc6b9fadc580311ec730931271d4411c0d1466
Justin Singh <justin.singh125125@gmail.com>
2019-08-08 14:24:14 -0700

    touching up

--- src/components/Certificate/index.js
+++ src/components/Certificate/index.js
@@ -10,14 +10,14 @@ const Certificate = props => {
 
 
         <div className="certificate">
-            <Fade left duration={1500} >
-                <img className="certificate__img" src="https://credlyapp.s3.amazonaws.com/badges/aa57dc61c96a888ea204005ab7ef7028.png" alt="Justin Singh's Certificate" />
-                <h2 className="certificate__heading">Full Stack Web Developer</h2>
-                <Fade left duration={1500} > <h3 className="certificate__sub-heading">UC Davis Division of Continuing and Professional Education</h3> </Fade>
-                <h5 className="certificate__name"> <span className="certificate__recipient">Recipient: </span> Justin Singh</h5>
-                <h5 className="certificate__date"><span className="certificate__issued">  Issued: &nbsp;&nbsp;&nbsp;&nbsp;</span>4/8/2019</h5>
-                <Fade left duration={1500} >   <a href="https://ucdavis.credly.com/member-badges/17347457" className="certificate__link btn btn--green" target="_blank">View Certificate</a> </Fade>
-            </Fade>
+
+            <img className="certificate__img" src="https://credlyapp.s3.amazonaws.com/badges/aa57dc61c96a888ea204005ab7ef7028.png" alt="Justin Singh's Certificate" />
+            <h2 className="certificate__heading">Full Stack Web Developer</h2>
+            <Fade left duration={1500} > <h3 className="certificate__sub-heading">UC Davis Division of Continuing and Professional Education</h3> </Fade>
+            <h5 className="certificate__name"> <span className="certificate__recipient">Recipient: </span> Justin Singh</h5>
+            <h5 className="certificate__date"><span className="certificate__issued">  Issued: &nbsp;&nbsp;&nbsp;&nbsp;</span>4/8/2019</h5>
+            <Fade left duration={1500} >   <a href="https://ucdavis.credly.com/member-badges/17347457" className="certificate__link btn btn--green" target="_blank">View Certificate</a> </Fade>
+
         </div >
 
     )
