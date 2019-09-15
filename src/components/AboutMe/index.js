8e9b7873f1d0a9cf386d950b04cf2b6720203f27
Justin Singh <justin.singh125125@gmail.com>
2019-09-14 18:33:07 -0700

    added new resume + revised work experience

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -17,7 +17,7 @@ const AboutMe = props => {
             <div className="about-me">
                 <h2 className="about-me__text">About Me</h2>
                 <p className="about-me__info">
-                    Hello, I am a 20 year old certified Full Stack Web Developer with a background in Computer Science and a lifelong dedication to learning.
+                    Hello, I am certified Full Stack Web Developer with a background in Computer Science.
                     I specialize in using the MERN Stack and I am experienced in the technologies found below! When I am free, I value time with friends and family. When I am not spending time
                     with them I also like to continuously educate myself with new technologies because I love to learn. I feel that there is always a way for someone to improve themselves and I
                     find that I can continusously improve myself through my passion for learning.
